9bffb407ae250545d15935d30e51cb44c43d51df
闫肖肖 <yanxiaoxiao@daoxila.com>
2018-05-04 09:43:13 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -18,7 +18,7 @@
       <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
     </form>
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+    <a href="/pages/counter/main" class="counter">前往到喜啦活动页面</a>
   </div>
 </template>
 
